140ea7ad5621ab2a2f8c77979a8c38761ed7627e
ravikumar <ravicodeproject@gmail.com>
2022-06-17 23:25:15 +0530

    created component folder to place all components here

--- projects/my-play-book/src/app/app.module.ts
+++ projects/my-play-book/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MyGuideComponent } from './my-guide/my-guide.component';
+import { MyGuideComponent } from './component/my-guide/my-guide.component';
 
 @NgModule({
   declarations: [
