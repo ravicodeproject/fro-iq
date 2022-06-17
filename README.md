0d67416bdf6587d56b30a64fa8c1abc0c588aa25
ravikumar <ravicodeproject@gmail.com>
2022-06-18 00:49:32 +0530

    Install and configure bootstrap using ngx-formly package

--- README.md
+++ README.md
@@ -25,3 +25,8 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+## Required Packages
+
+Run the following command to install ngx-formly with bootstrap theme `npm install @angular/forms @ngx-formly/core @ngx-formly/bootstrap --save`
+For more information click on following link [Formly Getting Started](https://formly.dev/guide/getting-started)
--- projects/my-play-book/src/app/app.module.ts
+++ projects/my-play-book/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
+import { FormlyModule } from '@ngx-formly/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +15,10 @@ import { MyGuideComponent } from './component/my-guide/my-guide.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormlyModule.forRoot(),
+    FormlyBootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
