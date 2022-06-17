d14e029a13d945634806164a7a7475bffe400573
ravikumar <ravicodeproject@gmail.com>
2022-06-17 23:34:31 +0530

    added a route for my-guide component

--- projects/my-play-book/src/app/app-routing.module.ts
+++ projects/my-play-book/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { MyGuideComponent } from './component/my-guide/my-guide.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:MyGuideComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
