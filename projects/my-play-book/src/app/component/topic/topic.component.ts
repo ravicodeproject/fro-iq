331658262522cd5f32461e9dcaff07695926ce1f
myplace-e-hub <ravicodeproject@gmail.com>
2022-07-02 20:49:48 +0530

    load formly json and render it

--- projects/my-play-book/src/app/component/topic/topic.component.ts
+++ projects/my-play-book/src/app/component/topic/topic.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { FormlyFieldConfig } from '@ngx-formly/core';
+import { default as TypesFormlyFields } from './config/topic.formly.json';
+import { cloneDeep as _cloneDeep} from 'lodash';
 
 @Component({
   selector: 'app-topic',
@@ -12,7 +14,7 @@ export class TopicComponent implements OnInit {
   
   form = new FormGroup({});
   model = { email: 'email@gmail.com' };
-
+  fields: FormlyFieldConfig[] = [];
   constructor(private route:ActivatedRoute) { 
     console.log(route.params);
     console.log(route.snapshot.params['bookname']);
@@ -20,19 +22,9 @@ export class TopicComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.fields = _cloneDeep(TypesFormlyFields);    
+    console.log(this.fields);
   }
- 
-  fields: FormlyFieldConfig[] = [
-    {
-      key: 'email',
-      type: 'input',
-      templateOptions: {
-        label: 'Email address',
-        placeholder: 'Enter email',
-        required: true,
-      }
-    }
-  ];
 
   onSubmit(model: any) {
     console.log(model);
